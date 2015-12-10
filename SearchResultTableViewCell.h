909703ae8c50f8ff368745d9f56eee922c72c915
lynnkuh <kuhlman.lynn@gmail.com>
2015-12-10 13:15:40 -0800

    Added SearchResultTableViewCell code

---
+++ SearchResultTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  SearchResultTableViewCell.h
+//  StackOverflowClient
+//
+//  Created by Regular User on 12/10/15.
+//  Copyright © 2015 Lynn Kuhlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Question.h"
+
+@interface SearchResultTableViewCell : UITableViewCell
+
+@property Question *question;
+
+@end
